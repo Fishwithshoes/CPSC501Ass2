53701bacda22ef16b3408a2f77b239c3d9dc2b19
Conlan <cjhanwel@ucalgary.ca>
2016-10-28 14:41:20 -0600

    added final unit testing, created TestClass5 to assist

---
+++ src/ut/TestClass5.java
@@ -0,0 +1,17 @@
+package ut;
+
+public class TestClass5 {
+	public boolean testBool = false;
+	public char testChar = 'f';
+	public byte testByte = 4;
+	public short testShort = 2;
+	public int testInt = 343;
+	public long testLong = 11456;
+	public float testFloat = 0.357f;
+	public double testDouble = 5.5645;
+	
+	public String[] testArray1 = {"test", "this", "array"};
+	
+	public TestClass3 testObject = new TestClass3();
+
+}
--- src/ut/TestInspector.java
+++ src/ut/TestInspector.java
@@ -2,16 +2,218 @@ package ut;
 
 import code.Inspector;
 import ut.TestClass1;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Field;
+
 public class TestInspector {
+
+	
+	/*private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+	    System.setOut(new PrintStream(output));
+	}
+
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	}*/
+
 	
 	Inspector testInspector = new Inspector();
 
-	@Test
-	public void testRecursion() {
+	
+	/*NOTE: All display methods are handled by testInspect(). I attempted to compare the output
+	//of each display method against a ByteArrayOutputStream, but was unsuccessful in getting proper
+	assertions*/
+	
+	/*@Test
+	public void testInspect() {
 		TestClass1 testObject = new TestClass1();
 		testInspector.inspect(testObject, true);
+	}*/
+	
+	/*NOTE: The following test both the primitiveHander switch method, and each String handler method 
+	per type*/
+	
+	@Test
+	public void testPrimitiveHandlerBool() {
+		TestClass5 testObject2 = new TestClass5();
+		Field testField = null;
+		try {
+			testField = testObject2.getClass().getField("testBool");
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals("false", testInspector.primitiveHandler(testField, testObject2));
 	}
+	
+	@Test
+	public void testPrimitiveHandlerChar() {
+		TestClass5 testObject2 = new TestClass5();
+		Field testField = null;
+		try {
+			testField = testObject2.getClass().getField("testChar");
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals("f", testInspector.primitiveHandler(testField, testObject2));
+	}
+	
+	@Test
+	public void testPrimitiveHandlerByte() {
+		TestClass5 testObject2 = new TestClass5();
+		Field testField = null;
+		try {
+			testField = testObject2.getClass().getField("testByte");
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals("4", testInspector.primitiveHandler(testField, testObject2));
+	}
+	
+	@Test
+	public void testPrimitiveHandlerShort() {
+		TestClass5 testObject2 = new TestClass5();
+		Field testField = null;
+		try {
+			testField = testObject2.getClass().getField("testShort");
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals("2", testInspector.primitiveHandler(testField, testObject2));
+	}
+	
+	@Test
+	public void testPrimitiveHandlerInt() {
+		TestClass5 testObject2 = new TestClass5();
+		Field testField = null;
+		try {
+			testField = testObject2.getClass().getField("testInt");
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals("343", testInspector.primitiveHandler(testField, testObject2));
+	}
+	
+	@Test
+	public void testPrimitiveHandlerLong() {
+		TestClass5 testObject2 = new TestClass5();
+		Field testField = null;
+		try {
+			testField = testObject2.getClass().getField("testLong");
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals("11456", testInspector.primitiveHandler(testField, testObject2));
+	}
+	
+	@Test
+	public void testPrimitiveHandlerFloat() {
+		TestClass5 testObject2 = new TestClass5();
+		Field testField = null;
+		try {
+			testField = testObject2.getClass().getField("testFloat");
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals("0.357", testInspector.primitiveHandler(testField, testObject2));
+	}
+	
+	@Test
+	public void testPrimitiveHandlerDouble() {
+		TestClass5 testObject2 = new TestClass5();
+		Field testField = null;
+		try {
+			testField = testObject2.getClass().getField("testDouble");
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals("5.5645", testInspector.primitiveHandler(testField, testObject2));
+	}
+
+	//this test also tests the array String generation handler
+	@Test
+	public void testArrayHandler() {
+		TestClass5 testObject2 = new TestClass5();
+		Field testField = null;
+		Object testArray = null;
+		try {
+			testField = testObject2.getClass().getField("testArray1");
+			testArray = testField.get(testObject2);
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals("test this array ", testInspector.arrayHandler(testArray, testObject2));
+	}
+	
+	/*NOTE: This test may fail due to package stubs not being present. Change below package stub to current
+	package, then re-test*/
+	@Test
+	public void testObjectHandler() {
+		TestClass5 testObject2 = new TestClass5();
+		Field testField = null;
+		try {
+			testField = testObject2.getClass().getField("testObject");
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals("ut.TestClass3@255316f2", testInspector.objectHandler(testField, testObject2));
+		//			  ^this stub
+	}
+	
 }
